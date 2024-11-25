5a60aef462c94986359ada1876bf5351498a0d7f
petrero <petrero@gmail.com>
2024-11-25 11:35:46 +0200

    2.4 Now, let's make this a lot better, because we want the user to be able to change the name themselves. So let's add an input element. I'm going to use w-full. We'll have some padding, a border. It'll have rounded corners. Let's give it a background color of gray. And then the text needs to be white so that we can see it. So we will have this input element.

--- resources/views/livewire/greeter.blade.php
+++ resources/views/livewire/greeter.blade.php
@@ -2,6 +2,11 @@
   <div>
     Hello, {{ $name }}
   </div>
+
+  <div class="mt-2">
+    <input type="text" class="block w-full p-4 border rounded-md bg-gray-700 text-white">
+  </div>
+
   <div class="mt-2">
     <button
         class="text-white font-medium rounded-md px-4 py-2 bg-blue-600"
