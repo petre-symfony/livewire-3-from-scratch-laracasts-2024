76fbca05cbdbba6aa4170b2c4a0b81d657f2d420
petrero <petrero@gmail.com>
2024-11-25 11:45:01 +0200

    2.6 But then there's people like me who like to keep their hands on the keyboard as much as possible. So I want to be able to type the name and then press Enter in order to change the name. Now, we can easily accommodate that functionality by using a form because, well, that's the default functionality of a form. All we have to do is set the buttons type to submit and then wrap all of this within a form. But by doing this, we don't necessarily need to listen for the click event on the button. We need to listen for the submit event on our form. And we can do that because when it comes to live wire actions, we can listen for just about any event.

--- resources/views/livewire/greeter.blade.php
+++ resources/views/livewire/greeter.blade.php
@@ -3,19 +3,21 @@
     Hello, {{ $name }}
   </div>
 
-  <div class="mt-2">
-    <input
-      id="newName"
-      type="text" class="block w-full p-4 border rounded-md bg-gray-700 text-white"
-    >
-  </div>
+  <form wire:click="changeName(document.querySelector('#newName').value)">
+    <div class="mt-2">
+      <input
+        id="newName"
+        type="text" class="block w-full p-4 border rounded-md bg-gray-700 text-white"
+      >
+    </div>
 
-  <div class="mt-2">
-    <button
-        class="text-white font-medium rounded-md px-4 py-2 bg-blue-600"
-        wire:click="changeName(document.querySelector('#newName').value)"
-    >
-      Greet
-    </button>
-  </div>
+    <div class="mt-2">
+      <button
+          type="submit"
+          class="text-white font-medium rounded-md px-4 py-2 bg-blue-600"
+      >
+        Greet
+      </button>
+    </div>
+  </form>
 </div>
