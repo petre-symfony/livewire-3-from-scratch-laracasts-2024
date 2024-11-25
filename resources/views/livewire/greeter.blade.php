f5c1e4f62735a163d3c5faf807119fc7e8f9f334
petrero <petrero@gmail.com>
2024-11-25 11:40:22 +0200

    2.5 But then the question becomes, how do we get the value from the input element and pass that to our change name action? Well, the most straightforward thing to me is to just add an ID. We can call this new name so that inside of our click event handler, we could use document query selector. We want to select the element with an ID of new name and get its value. So if we type a value inside of our text box and click greet, we can see that the name changes. Let's change it to James and John. And that functionality is fine for most people.I mean, most people are going to type a name and then click on the button.

--- resources/views/livewire/greeter.blade.php
+++ resources/views/livewire/greeter.blade.php
@@ -4,13 +4,16 @@
   </div>
 
   <div class="mt-2">
-    <input type="text" class="block w-full p-4 border rounded-md bg-gray-700 text-white">
+    <input
+      id="newName"
+      type="text" class="block w-full p-4 border rounded-md bg-gray-700 text-white"
+    >
   </div>
 
   <div class="mt-2">
     <button
         class="text-white font-medium rounded-md px-4 py-2 bg-blue-600"
-        wire:click="changeName('Jeffrey Cox')"
+        wire:click="changeName(document.querySelector('#newName').value)"
     >
       Greet
     </button>
