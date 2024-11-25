fa3a238c90e1b5cf42895a87b04c08dc7476b635
petrero <petrero@gmail.com>
2024-11-25 11:23:47 +0200

    2.2 That's perfectly fine. But of course, that doesn't answer our question, how do we execute our action? But it's actually quite simple. In a typical application, we would listen for the click event. But with LiveWire, we say wire colon click. And then the value is the name of the action that we want to execute, which is changeName in this case. So whenever I click on the Greet button, we should see the name changed to Jeffrey. And we can just refresh. We can click Greet. And we can keep doing this all day long. But of course, that's not very useful. Instead, we would want to be able to change the name to really anything.

--- resources/views/livewire/greeter.blade.php
+++ resources/views/livewire/greeter.blade.php
@@ -3,7 +3,10 @@
     Hello, {{ $name }}
   </div>
   <div class="mt-2">
-    <button class="text-white font-medium rounded-md px-4 py-2 bg-blue-600">
+    <button
+        class="text-white font-medium rounded-md px-4 py-2 bg-blue-600"
+        wire:click="changeName()"
+    >
       Greet
     </button>
   </div>
