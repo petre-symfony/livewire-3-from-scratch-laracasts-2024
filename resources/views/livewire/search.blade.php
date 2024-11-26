b03acda6f2c9b424d0773d2698a1727b4ecd9d39
petrero <petrero@gmail.com>
2024-11-26 11:59:56 +0200

    6.5 Let's also open up the greeter blade view because this gives us a pretty good starting point at least as far as the form is concerned. We of course do need to make a few changes. Like for example we aren't going to be submitting this form because the text box is going to be the only input element. We aren't going to have a select. We aren't even going to have a button. So let's get rid of the error message. Let's get rid of the button. And let's add a class w-full to the input element just so that it will span the width of the page. But of course we need to open up the welcome view so that we can use our new search component instead of the greeter. So we'll just make that change to search and now that's great. But I do want a placeholder here just so that we know that this is a place to type something to search.

--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -1,3 +1,10 @@
 <div>
-  {{-- A good traveler has no fixed plans and is not intent upon arriving. --}}
+  <form>
+    <div class="mt-2">
+      <input
+          type="text" class="p-4 w-full border rounded-md bg-gray-700 text-white"
+          placeholder="Type something to search"
+      >
+    </div>
+  </form>
 </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -873,7 +873,7 @@
           </header>
 
           <main class="mt-6">
-            <livewire:greeter>
+            <livewire:search/>
           </main>
 
         </div>
