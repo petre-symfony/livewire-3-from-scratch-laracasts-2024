db453b15dc142950c056c76a83973db61bbe70fd
petrero <petrero@gmail.com>
2024-11-26 10:43:21 +0200

    4.6 But there is another way. We can use an attribute called validate. This comes from LiveWire attributes, and all we have to do is supply the rules. So our name property is required. It has a minimum length of two. And I like this approach because it's very clear. This is the property. These are the validation rules for that property. I like it. It also has the side effect that LiveWire will validate that property for every update. So if I come to the browser and I have just a single character here and I click on greet, we see that the name field must be at least two characters. And so it validated that value, but also notice what happened here. We have a greeting message. Now, if you think about it, this makes perfect sense because if you look at the change greeting method, we are setting a new value for greeting message. We are taking the value from greeting and the value from name and creating our greeting message. Nowhere inside of this method does it say to not do this if validation fails. Now, yes, I commented out the line of code where we actually validate, but I wanted to point out that LiveWire is going to automatically validate properties if you use the validate attribute. That doesn't necessarily mean that it's going to stop the code from executing if validation fails, but it will validate it.

--- app/Livewire/Greeter.php
+++ app/Livewire/Greeter.php
@@ -2,9 +2,11 @@
 
 namespace App\Livewire;
 
+use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 class Greeter extends Component {
+	#[Validate('required|min:2')]
 	public $name = '';
 	public $greeting = '';
 	public $greetingMessage = '';
@@ -12,16 +14,18 @@ class Greeter extends Component {
 	public function changeGreeting() {
 		$this->reset('greetingMessage');
 
-		$this->validate();
-		
+		//$this->validate();
+
 		$this->greetingMessage = "{$this->greeting}, {$this->name}!";
 	}
 
+	/**
 	public function rules() {
 		return [
 			'name' => 'required|min:2'
 		];
 	}
+  */
 
 	public function render() {
 		return view('livewire.greeter');
