af5454bce9ea5c25437637aae1b41f88196981d0
petrero <petrero@gmail.com>
2024-12-01 00:13:06 +0200

    8.11 The results are inside of a block level element. So we need to change this markup or at least the styling of it. So for this div element, let's add some padding. Let's make it absolutely positioned so that it is out of the flow of the document. Let's add a border. Let's make it rounded to match everything else. The background is going to be gray 700 because I believe that's what I used as the background for the input element. Yes, it is. And then the border is going to be indigo 600 to match the background color of the button. Except that that needs to be border indigo 600.  So now whenever we view this in the browser, now we see that element always in the browser, even if there aren't any results to display. So we need to hide that by default.

--- resources/views/livewire/search-results.blade.php
+++ resources/views/livewire/search-results.blade.php
@@ -1,5 +1,5 @@
 <div>
-  <div class="mt-4">
+  <div class="mt-4 p-4 absolute border rounded-md bg-gray-700 border-indigo-600">
     @foreach($results as $result)
       <div class="pt-2">
         <a href="/articles/{{ $result->id }}">
