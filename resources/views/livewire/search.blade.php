5f3a3ed079c3252000f9ec4584a376f24a607792
petrero <petrero@gmail.com>
2024-12-01 00:05:19 +0200

    8.7 So inside of our search component, we will say livewire:search-results, and that's going to display the results. But we do get an error for each argument must be of type array object.

--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -16,13 +16,5 @@
     </div>
   </form>
 
-  <div class="mt-4">
-    @foreach($results as $result)
-      <div class="pt-2">
-        <a href="/articles/{{ $result->id }}">
-          {{ $result->title }}
-        </a>
-      </div>
-    @endforeach
-  </div>
+  <livewire:search-results>
 </div>
