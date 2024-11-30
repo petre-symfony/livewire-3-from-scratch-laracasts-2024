8c190c2a4a2683578f16ec1d582c4d248450729a
petrero <petrero@gmail.com>
2024-12-01 00:03:09 +0200

    8.6 And let's open up those files. Let's start with the view because we can take the markup from our search component and lift that out and almost directly just paste it here inside of our search results view. Now, of course, this means that we need a property called results. So let's open up search results and let's just add that public property. And then we will use this component just like we would any other.

--- app/Livewire/SearchResults.php
+++ app/Livewire/SearchResults.php
@@ -5,6 +5,8 @@ namespace App\Livewire;
 use Livewire\Component;
 
 class SearchResults extends Component {
+	public $results;
+
 	public function render() {
 		return view('livewire.search-results');
 	}
