aadf9e2a9507f116a14cc3261ce1c59ab446d761
petrero <petrero@gmail.com>
2024-12-01 00:10:50 +0200

    8.10 But inside of our search results class, we can mark the results property as reactive with the reactive attribute. And that's all that we have to do. So if we go back to the browser and perform a search, we will now see our results are displayed. But notice that everything shifted down. And that's because, well, that's what it does.

--- app/Livewire/SearchResults.php
+++ app/Livewire/SearchResults.php
@@ -2,9 +2,11 @@
 
 namespace App\Livewire;
 
+use Livewire\Attributes\Reactive;
 use Livewire\Component;
 
 class SearchResults extends Component {
+	#[Reactive]
 	public $results = [];
 
 	public function render() {
