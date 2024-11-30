6e79a134fc551ad130aee7a48b1f175674d3851d
petrero <petrero@gmail.com>
2024-11-30 22:33:36 +0200

    6.6 So with that in place we can close everything else because all we need is our view and then we need the search class. So really there's only two things that we need to be working with as far as our component is concerned. We will need the search text which we can initialize as an empty string. Then we will need our search results which we will initialize as an empty array.  Now we do need some validation here. So let's use the validate attribute and really we only need to be sure that we have something to work with because we should be able to let the user search with a single character if that were the case. So all we need is required.

--- app/Livewire/Search.php
+++ app/Livewire/Search.php
@@ -2,9 +2,14 @@
 
 namespace App\Livewire;
 
+use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 class Search extends Component {
+	#[Validate('required')]
+	public $searchText = '';
+	public $results = [];
+
 	public function render() {
 		return view('livewire.search');
 	}
