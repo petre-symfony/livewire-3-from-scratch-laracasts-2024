a241164903845ea2aa01b45d0dc5d81432668006
petrero <petrero@gmail.com>
2024-11-30 22:35:17 +0200

    6.7 Now because our form does not have a button to actually submit the search we are going to need to do something when the user types into that input element. So we need to change the model to search text. I'm going to add the live modifier but we don't want this running for every keystroke. Just like we learned a couple of lessons ago we can debounce this. So let's do that. I think the default value for debounce is going to be fine. So I'm not going to specify that anymore with another modifier.

--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -4,6 +4,7 @@
       <input
           type="text" class="p-4 w-full border rounded-md bg-gray-700 text-white"
           placeholder="Type something to search"
+          wire:model.live.debounce="searchText"
       >
     </div>
   </form>
