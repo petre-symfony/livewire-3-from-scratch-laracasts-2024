9e4b4516354e0bd2c86706ce1d3cf3de7e693f5b
petrero <petrero@gmail.com>
2024-12-01 00:00:40 +0200

    8.5 Now one thing to remember is that live wire components are blade components. So everything that we know about blade components can almost be transferred to live wire components, such as passing data. Here we are passing static data, a string to a property called placeholder. That would work for a blade component. And we will talk more about passing data.  But for right now, I want to essentially break up our search component into two components, I want the first component to still be our search component, but it's just going to contain the UI, the input element and the button. Then we will extract the display of the results to another component. So let's go ahead and create that other component.  We will make live wire and let's just call it search results.   php artisan make:livewire search-results

---
+++ app/Livewire/SearchResults.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class SearchResults extends Component {
+	public function render() {
+		return view('livewire.search-results');
+	}
+}
