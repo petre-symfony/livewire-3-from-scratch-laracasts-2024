e3c2c0c227173e340beead85bc5ab5cd0ff3e665
petrero <petrero@gmail.com>
2024-12-01 10:37:06 +0200

    9.1 Events are probably the most important concept when it comes to building graphical applications, because it's how we interact with the user. But in the case of components, we can use events to communicate from one component to another. In fact, we can communicate with as many components throughout the entire page that we need, because well, that's the nature of events. So I want to make a few more changes to our search component. Let's go ahead and let's get rid of the clear button so that we have something like what we began with originally. So let's get rid of that. Let's change the width back to full.

--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -2,17 +2,10 @@
   <form>
     <div class="mt-2">
       <input
-          type="text" class="p-4 w-9/12 border rounded-md bg-gray-700 text-white"
+          type="text" class="p-4 w-full border rounded-md bg-gray-700 text-white"
           placeholder="{{ $placeholder }}"
           wire:model.live.debounce="searchText"
       >
-
-      <button class="text-white font-medium rounded-md p-4 bg-indigo-600 disabled:bg-indigo-400"
-              wire:click.prevent="clear()"
-              {{empty($searchText) ? 'disabled' : ''}}
-      >
-        Clear
-      </button>
     </div>
   </form>
 
