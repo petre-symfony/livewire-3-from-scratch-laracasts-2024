dbae0bf414780e27776f19dc364d79a53610bf25
petrero <petrero@gmail.com>
2024-12-01 15:47:13 +0200

    11.4 And so now we just need to implement the ArticleIndex component. So let's open up the class, as well as the view. And we'll start with the class, because we need some data in order to display that. So let's start by adding a public property called articles. Let's initialize it as an empty array. So that inside of the mount method, we will simply set the articles to whatever we get from the database, we'll just fetch all of them. Now this is going to work, but there's actually a better way to go about this, which we will talk about here in a few moments.  For right now, let's just get this to where we can display our articles. So we will for each articles as article, let's go ahead and add the end for each statement here. We will surround each entry with a div, so that we can add some margin to the top and some padding. And I think that that's going to be enough. Because when it comes to displaying the list of articles, we want some separation between each entry. And I think that that's going to give us enough.  So that then we will have an h3, let's set the text to 2xl. The text is going to be blue 500, something other than white. Let's give it some margin on the bottom, so that we can add some visual separation between the h3, which is going to be the title, and then the content. And then we will have a hover effect, so that we will change the text to blue 700. Then inside of here, we will have an a element, so that we can navigate to the individual article. So we will use the ID there. We want our title, so we will use the title here. And then as far as the content is concerned, we'll just have a p element. I don't think we need anything else. And then we will simply output the content. So if we take a look at that, okay, that's getting better.  I want some margin on the sides. So here at our root div element, let's add some auto margin. Let's make the width of one half. And yeah, I like that. So if we navigate to the individual articles, we can see that.  But that's very jarring to go from one to the other. So let's add that same class to, what was that? That was article show, wasn't it? Or show article, something along those lines. There it is, show article. And we will add that class there. So now whenever we go, okay, that's good.

--- app/Livewire/ArticleIndex.php
+++ app/Livewire/ArticleIndex.php
@@ -2,9 +2,16 @@
 
 namespace App\Livewire;
 
+use App\Models\Article;
 use Livewire\Component;
 
 class ArticleIndex extends Component {
+	public $articles = [];
+
+	public function mount() {
+		$this->articles = Article::all();
+	}
+
 	public function render() {
 		return view('livewire.article-index');
 	}
--- resources/views/livewire/article-index.blade.php
+++ resources/views/livewire/article-index.blade.php
@@ -1,3 +1,10 @@
-<div>
-  {{-- Knowing others is intelligence; knowing yourself is true wisdom. --}}
+<div class="m-auto w-1/2">
+  @foreach($articles as $article)
+    <div class="mt-5 p-3">
+      <h3 class="text-2xl text-blue-500 mb-3 hover:text-blue-700">
+        <a href="/articles/{{ $article->id }}">{{ $article->title }}</a>
+      </h3>
+      <p>{{ $article->content }}</p>
+    </div>
+  @endforeach
 </div>
