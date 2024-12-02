84c1fec13d801870fa9b5bd301de648efbf71b3e
petrero <petrero@gmail.com>
2024-12-02 23:07:46 +0200

    13.19 So inside of our article form class, we'll paste in that stuff. We want our title and our content. We have the validate attributes still there. But for our save method, let's rename that to store. So that we will validate this data. We will save, or rather we will create our new article. And that's it. We don't really need to do anything else.

--- app/Livewire/Forms/ArticleForm.php
+++ app/Livewire/Forms/ArticleForm.php
@@ -2,9 +2,19 @@
 
 namespace App\Livewire\Forms;
 
+use App\Models\Article;
 use Livewire\Attributes\Validate;
 use Livewire\Form;
 
 class ArticleForm extends Form {
-	//
+	#[Validate('required')]
+	public $title;
+	#[Validate('required')]
+	public $content;
+
+	public function store() {
+		$this->validate();
+
+		Article::create($this->all());
+	}
 }
