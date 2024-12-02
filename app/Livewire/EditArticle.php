5b4cd9f243e9bf3f04466ee800f39ce640dcf309
petrero <petrero@gmail.com>
2024-12-02 22:46:27 +0200

    13.13 And for the most part, we can just take most of everything from our create article class. We don't need render because edit article needs its own render method, but we can paste in those properties.

--- app/Livewire/EditArticle.php
+++ app/Livewire/EditArticle.php
@@ -2,7 +2,23 @@
 
 namespace App\Livewire;
 
+use App\Models\Article;
+use Livewire\Attributes\Validate;
+
 class EditArticle extends AdminComponent {
+	#[Validate('required')]
+	public $title;
+	#[Validate('required')]
+	public $content;
+
+	public function save() {
+		$this->validate();
+
+		Article::create($this->all());
+
+		$this->redirect('/dashboard/articles', navigate: true);
+	}
+	
 	public function render() {
 		return view('livewire.edit-article');
 	}
