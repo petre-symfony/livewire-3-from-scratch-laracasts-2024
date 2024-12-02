fa8f29bd0ddf548e68926d51949abf057220dba2
petrero <petrero@gmail.com>
2024-12-02 22:52:32 +0200

    13.14 We need the validate attribute, but here we are editing. We are going to be getting an article to work with. So let's use the mount hook to where we will get our article that we will then use to set the title to the article title. We'll do the same thing for the content. And you know what? We should also just have a public article, and we'll just call it article.  So that we would have a reference to that, and we would be able to use that in order to update our data. So inside of save, we still want to validate our data, but we're not creating. Instead, we are using this article we will call update. We can pass in, really, we just only need the title and the content. And then we still want to redirect. So yeah, that should work just fine.

--- app/Livewire/EditArticle.php
+++ app/Livewire/EditArticle.php
@@ -6,19 +6,28 @@ use App\Models\Article;
 use Livewire\Attributes\Validate;
 
 class EditArticle extends AdminComponent {
+	public ?Article $article;
+
 	#[Validate('required')]
 	public $title;
 	#[Validate('required')]
 	public $content;
 
+	public function mount(Article $article) {
+		$this->title = $article->title;
+		$this->content = $article->content;
+
+		$this->article = $article;
+	}
+
 	public function save() {
 		$this->validate();
 
-		Article::create($this->all());
+		$this->article->update($this->only(['title', 'content']));
 
 		$this->redirect('/dashboard/articles', navigate: true);
 	}
-	
+
 	public function render() {
 		return view('livewire.edit-article');
 	}
