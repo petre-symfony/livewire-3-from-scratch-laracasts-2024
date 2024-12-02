28a973b26737d2c6cd9125d5da6b558bf4e72d03
petrero <petrero@gmail.com>
2024-12-02 18:28:57 +0200

    12.5 So let's set some titles for these components. Let's open up the dashboard component. Let's also open up the article index component. And we want to add a title, but we need the class, not the view. So there's article index. And we can add a title in a couple of different ways.  But probably the most straightforward thing to do is to just use the title attribute on our class. So this is the article index. We can just say articles, and that's going to be fine. We can go to our dashboard. We can add the title attribute here, and we'll say admin dashboard.  So now whenever we look at the browser, we have our titles for the articles and admin dashboard. That's a lot better.

--- app/Livewire/Dashboard.php
+++ app/Livewire/Dashboard.php
@@ -2,8 +2,10 @@
 
 namespace App\Livewire;
 
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
+#[Title('Admin Dashboard')]
 class Dashboard extends Component {
 	public function render() {
 		return view('livewire.dashboard');
