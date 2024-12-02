f99f0dd1b9bc37376893f550a3faa891169ccf66
petrero <petrero@gmail.com>
2024-12-02 21:15:26 +0200

    12.20 In this case, when our articles change, we provide a new list of articles to the view so that it can be re-rendered. So now if we click on Latin grammar, that one's going to go away and our list is automatically updated.

--- app/Livewire/ArticleList.php
+++ app/Livewire/ArticleList.php
@@ -5,17 +5,14 @@ namespace App\Livewire;
 use App\Models\Article;
 
 class ArticleList extends AdminComponent {
-	public $articles = [];
-
-	public function mount() {
-		$this->articles = Article::all();
-	}
-
+	
 	public function delete(Article $article) {
 		$article->delete();
 	}
 
 	public function render() {
-		return view('livewire.article-list');
+		return view('livewire.article-list', [
+			'articles' => Article::all()
+		]);
 	}
 }
