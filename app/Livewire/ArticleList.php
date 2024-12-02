fd00652db61cbb728f6c0463496f9217dea96772
petrero <petrero@gmail.com>
2024-12-02 21:13:36 +0200

    12.19 Let's look at this the other way. If we had the public articles as an empty array, and then we called the mount method to where we set this articles to article all. Let's get rid of where we provide that to the view. It's going to look the same. You know, there it is right there. We have the same thing. But whenever I click on this bottom one, I want to go after that savage queen. If we delete this, we don't get any kind of confirmation that yes, it was deleted. The list is not automatically updated. If we refresh, then we can see that it's gone.  So this is why it's really best to avoid this kind of pattern. Because as our data updates, we are not automatically updating what is rendered inside of the page. Because we quite explicitly said, don't change anything as far as rendering.

--- app/Livewire/ArticleList.php
+++ app/Livewire/ArticleList.php
@@ -5,13 +5,17 @@ namespace App\Livewire;
 use App\Models\Article;
 
 class ArticleList extends AdminComponent {
+	public $articles = [];
+
+	public function mount() {
+		$this->articles = Article::all();
+	}
+
 	public function delete(Article $article) {
 		$article->delete();
 	}
 
 	public function render() {
-		return view('livewire.article-list', [
-			'articles' => Article::all()
-		]);
+		return view('livewire.article-list');
 	}
 }
