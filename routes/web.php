bc8203304e09a476a865fb52c8759a512596628a
petrero <petrero@gmail.com>
2024-12-02 22:35:33 +0200

    13.10 So let's go ahead and create that inside of our web.php file. Let's set up the route for the edit article component, except that the URL needs to be a little bit different. We'll have dashboard slash articles. Then we would have the article that we want to work with, and then slash edit. And we are done with our route file, so we can close that.

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Livewire\ArticleIndex;
 use App\Livewire\ArticleList;
 use App\Livewire\CreateArticle;
 use App\Livewire\Dashboard;
+use App\Livewire\EditArticle;
 use App\Livewire\Search;
 use App\Livewire\ShowArticle;
 use Illuminate\Support\Facades\Route;
@@ -18,6 +19,7 @@ Route::get('/articles/{article}', ShowArticle::class);
 Route::get('/dashboard', Dashboard::class);
 Route::get('/dashboard/articles', ArticleList::class);
 Route::get('/dashboard/articles/create', CreateArticle::class);
+Route::get('/dashboard/articles/{article}/edit', EditArticle::class);
 
 /**
  Route::middleware([
