93341863952ec8099de91ba9275b33fcae4c4f21
petrero <petrero@gmail.com>
2024-12-02 22:16:33 +0200

    13.4 And let's open up create article because we already kind of know what we are going to be working with. We have the title, and we also have the content. And we can go ahead and set up the validation rules here.  So let's do that using the validate attribute. And I think all that we really need is required. Ideally, we would have like a minimum length. But for our purposes, this is going to be just fine. And since we're here, we can go ahead and write a method to save our data.  So the first thing we want to do is validate our data so that then we can use our article model to create. And we could just pass in all of our component data. That title and the content would be there. And then we would want to redirect back to the dashboard article list.

--- app/Livewire/CreateArticle.php
+++ app/Livewire/CreateArticle.php
@@ -2,9 +2,24 @@
 
 namespace App\Livewire;
 
+use App\Models\Article;
+use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 class CreateArticle extends Component {
+	#[Validate('required')]
+	public $title;
+	#[Validate('required')]
+	public $content;
+
+	public function save() {
+		$this->validate();
+
+		Article::create($this->all());
+
+		$this->redirect('/dashboard/articles');
+	}
+
 	public function render() {
 		return view('livewire.create-article');
 	}
