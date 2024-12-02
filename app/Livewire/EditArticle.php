c5e140fbed361b6f3a4b265bbee3d9aeac65b760
petrero <petrero@gmail.com>
2024-12-02 23:16:22 +0200

    13.23 Instead, we have that public article form form. So that inside of mount, we can use our form object. We could have, you know, something similar to mount. But let's not call it mount. Let's call it set article. And then we will pass in that article. And that's really all that we need to do there.

--- app/Livewire/EditArticle.php
+++ app/Livewire/EditArticle.php
@@ -2,12 +2,15 @@
 
 namespace App\Livewire;
 
+use App\Livewire\Forms\ArticleForm;
 use App\Models\Article;
 use Livewire\Attributes\Validate;
 
 class EditArticle extends AdminComponent {
+	public ArticleForm $form;
+
 	public function mount(Article $article) {
-		
+		$this->form->setArticle($article);
 	}
 
 	public function save() {
