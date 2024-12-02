845c2e0c6dc450f374b266a55d5382696ff20136
petrero <petrero@gmail.com>
2024-12-02 21:11:20 +0200

    12.18 So we need to implement that inside of our class. So let's add that public function delete. Now we are getting an ID, but really we can type in this. We can say that we are going to get an article and we can just call it article. This way it simplifies it for us so that we can just call the delete method and that should work. So if we go back to the browser, let's scroll all the way down.  And this bottom one, it quite makes my forehead ache. Yeah, I've been there. Let's click on delete and that should go away. Now, I want you to notice that the list of articles automatically updated. That's because our articles changed. So it re-rendered and a new set of articles was provided to the view.

--- app/Livewire/ArticleList.php
+++ app/Livewire/ArticleList.php
@@ -3,9 +3,12 @@
 namespace App\Livewire;
 
 use App\Models\Article;
-use Livewire\Component;
 
 class ArticleList extends AdminComponent {
+	public function delete(Article $article) {
+		$article->delete();
+	}
+
 	public function render() {
 		return view('livewire.article-list', [
 			'articles' => Article::all()
