4ede3fd32f68946c1b7c6cfcf478604be0c86e65
petrero <petrero@gmail.com>
2024-12-03 19:32:50 +0200

    16.1 Working with Dirty States  When you work with forms, you will eventually need to work with dirty states. Now, if you're not familiar with that term, it's basically just when the form data has changed from its original value. It can be at the form level, or we can apply that same terminology to the individual form fields. And implementing your own dirty tracking system is, but thankfully Livewire has this covered. It's built in and it's very easy to use.  At the most basic level, we want to display a message when the form data has changed. So let's add a div and let's say that the form data has changed. Now of course, if we were to view this in the browser, we would see this message because well it's right here.  We're not hiding it or anything, but we only want to display this when the form data has changed. And we can do so with the wire:dirty attribute. This hides the element that it is applied to and it will only show it when the data has changed. So that can be for the title, or it could be for the content, or the published value, or even the notification options. It doesn't matter. If the data is different, the form is dirty, and therefore our message has changed.

--- resources/views/livewire/create-article.blade.php
+++ resources/views/livewire/create-article.blade.php
@@ -34,24 +34,38 @@
     <div class="mb-3">
       <div>
         <div class="mb-2">Notification Options</div>
-        <div class="flex gap-6">
+        <div class="flex gap-6 mb-3">
           <label class="flex items-center">
-            <input type="radio" value="email" class="mr-2"
-                   wire:model="form.notification"
+            <input type="radio" value="true" class="mr-2"
+                   wire:model.boolean="form.allowNotifications"
+            >
+            Yes
+          </label>
+          <label class="flex items-center">
+            <input type="radio" value="false" class="mr-2"
+                   wire:model.boolean="form.allowNotifications"
+            >
+            No
+          </label>
+        </div>
+        <div x-show="$wire.form.allowNotifications">
+          <label class="flex items-center">
+            <input type="checkbox" value="email" class="mr-2"
+                   wire:model="form.notifications"
             >
             Email
           </label>
           <label class="flex items-center">
-            <input type="radio" value="sms" class="mr-2"
-                   wire:model="form.notification"
+            <input type="checkbox" value="sms" class="mr-2"
+                   wire:model="form.notifications"
             >
             SMS
           </label>
           <label class="flex items-center">
-            <input type="radio" value="none" class="mr-2"
-                   wire:model="form.notification"
+            <input type="checkbox" value="push" class="mr-2"
+                   wire:model="form.notifications"
             >
-            None
+            Push
           </label>
         </div>
       </div>
