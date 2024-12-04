136237e8b69adff45a522977a250ac0108dfa945
petrero <petrero@gmail.com>
2024-12-04 18:15:39 +0200

    18.8 So let's do this. We know that we have some placeholder text. So let's start off by providing some placeholder text. And we'll just say loading. So that's inside of what? That's the placeholder. Well, we need to revisit this. So let's do that. Let's change our div to a span. And I think as far as our placeholder, that's it.  But we do need to open up the published count view, which again, let's change this to a span. Let's get rid of this text that says published. All we really want is the count.

--- resources/views/livewire/article-list.blade.php
+++ resources/views/livewire/article-list.blade.php
@@ -9,7 +9,7 @@
         Show All
       </button>
       <button class="text-gray-200 p-2 bg-blue-700 hover:bg-blue-900 rounded-sm">
-        Show Published (<livewire:published-count />)
+        Show Published (<livewire:published-count placeholder-text="loading"/>)
       </button>
     </div>
   </div>
