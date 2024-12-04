c45f9964d1c733e76b6b102c89e6e7e0fb7bc303
petrero <petrero@gmail.com>
2024-12-04 17:08:49 +0200

    17.2 Now, I know there are many better ways of doing this. So this is for demonstration purposes. But here for the view, I'm going to have some placeholder content. So we'll just have published and zero.  So that's inside of the article list in our dashboard, we can include that published count component. So this is going to be at the top of the article list.  But I do know we will need to add flex, justify between, and items center. And in the browser, we can see that it looks okay up here. But I definitely want this table to be wider. I want it to be in line with the edge of the text.

--- resources/views/livewire/article-list.blade.php
+++ resources/views/livewire/article-list.blade.php
@@ -1,9 +1,10 @@
 <div class="m-auto w-1/2 mb-4">
-  <div class="mb-3">
+  <div class="mb-3 flex justify-between items-center">
     <a href="/dashboard/articles/create"
        class="text-gray-200 p-2 bg-indigo-700 hover:bg-indigo-900 rounded-sm"
        wire:navigate
     >Create Article</a>
+    <livewire:published-count />
   </div>
   <table>
     <thead class="text-xs uppercase bg-gray-700 text-gray-400">
