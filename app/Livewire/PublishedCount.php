4edc179e7b4b4b1b06e17a290278855e77de9255
petrero <petrero@gmail.com>
2024-12-04 17:38:19 +0200

    17.10 And there's one other way that we can specify for a component to be lazily loaded. And that is to use the lazy attribute.  And this is a little bit different because this forces your component to always be lazily loaded. Which I think we could do in this case, because it is always going to take three seconds to load this published count. In which case, we can go back to our article list and we can take the lazy attribute out. And in the browser, we see the same behavior. There's our placeholder and it's replaced with the final content. But now every time that we use published count, it will always be lazily loaded. And that's it. Livewire's lazy loading feature is very easy to use. You can lazily load any component by just adding the lazy attribute to the component tag. If you have a full page component, you can call the lazy method whenever you define the route. Or if you always want to lazily load a particular component, you can use the lazy attribute.

--- app/Livewire/PublishedCount.php
+++ app/Livewire/PublishedCount.php
@@ -3,8 +3,10 @@
 namespace App\Livewire;
 
 use App\Models\Article;
+use Livewire\Attributes\Lazy;
 use Livewire\Component;
 
+#[Lazy]
 class PublishedCount extends Component {
 	public $count = 0;
 
