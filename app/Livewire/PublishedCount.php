73c0ce4a8b9072eb1dae796f9e472ee03def4115
petrero <petrero@gmail.com>
2024-12-04 17:01:28 +0200

    17.1 Lazy loading  At some point in time, we have to be concerned with long running processes. It's just a part of writing applications. But for web apps, it's a little bit more apparent because these long running processes can affect the load time of our pages. And longer load times are never something that we want the users to see. Now, of course, this is not a new problem.  This is very old and there are many ways that we can tackle it. But Livewire has the ability to lazily load content. So I'm going to create a new component to display the amount or the number of published articles.   php artisan make:livewire published-count

---
+++ app/Livewire/PublishedCount.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class PublishedCount extends Component {
+	public function render() {
+		return view('livewire.published-count');
+	}
+}
