5cfeabaea6b666e46ce88396ed9d5a0dfcc7cc3a
petrero <petrero@gmail.com>
2024-12-06 21:24:11 +0200

    20.3 But we can't use the articles property like we did before. We have to use this articles. And I think we did this in three different places. So we did it for the links at the top, then whenever we iterate over the articles, and then finally at the bottom for the other links. And that's it. In the browser, we see our list of articles.

--- resources/views/livewire/article-list.blade.php
+++ resources/views/livewire/article-list.blade.php
@@ -18,7 +18,7 @@
     </div>
   </div>
   <div class="my-3">
-    {{ $articles->links() }}
+    {{ $this->articles->links() }}
   </div>
   <table class="w-full">
     <thead class="text-xs uppercase bg-gray-700 text-gray-400">
@@ -28,7 +28,7 @@
       </tr>
     </thead>
     <tbody>
-      @foreach($articles as $article)
+      @foreach($this->articles as $article)
         <tr wire:key="{{ $article->id }}" class="border-b bg-gray-800 border-gray-700">
           <td class="px-6 py-3">{{ $article->title }}</td>
           <td class="px-6 py-3">
@@ -49,4 +49,7 @@
       @endforeach
     </tbody>
   </table>
+  <div class="my-3">
+    {{ $this->articles->links() }}
+  </div>
 </div>
