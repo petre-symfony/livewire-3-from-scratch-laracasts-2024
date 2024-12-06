aded83ea4ce0e6d793ef960b0c3657ce47a4e38c
petrero <petrero@gmail.com>
2024-12-06 21:38:06 +0200

    20.10 But then there's a third option. We can actually cache a component across all instances of that component. Like, for example, our show published component. Every page load, we are fetching that fresh from the database. And I think that is something that we could cache.  So inside of our published count component, we'll get rid of the count property because we are going to create a computed property called count. And we're just going to replace mount with count. We do need to return the result of getting the count of published articles. And I think inside of the view, all we need to do is say this count.

--- app/Livewire/PublishedCount.php
+++ app/Livewire/PublishedCount.php
@@ -3,6 +3,7 @@
 namespace App\Livewire;
 
 use App\Models\Article;
+use Livewire\Attributes\Computed;
 use Livewire\Attributes\Lazy;
 use Livewire\Component;
 
@@ -11,10 +12,11 @@ class PublishedCount extends Component {
 	public $count = 0;
 	public $placeholderText = '';
 
-	public function mount() {
+	#[Computed]
+	public function count() {
 		sleep(1);
 
-		$this->count = Article::where('published', 1)->count();
+		return Article::where('published', 1)->count();
 	}
 
 	public function placeholder() {
