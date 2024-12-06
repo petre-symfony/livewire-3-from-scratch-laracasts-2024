9f3f7e42829161e7d59d62900bc6ed889fa20182
petrero <petrero@gmail.com>
2024-12-06 18:26:04 +0200

    20.1 Computed Properties  LiveWire has computed properties. And you can think of them as accessors on model classes. They are, well, they are essentially methods that we can use to access data. And that data is cached. And we can easily create a computed property by just defining a method inside of our component class. The name of that method needs to be the name of the computed property that we want to use. And then we just decorate it with the computed attribute.  Now, I've called this articles because I'm going to change how we provide articles to the view. I want to cache all of this.  So we're going to take the code from the render method, and we're going to paste that into articles. So we are still going to build our query. We are going to determine if we want to show the published or all of them. But when it comes to returning, we want to return our paginated results. And then it is that that is going to be cached.

--- app/Livewire/ArticleList.php
+++ app/Livewire/ArticleList.php
@@ -3,12 +3,24 @@
 namespace App\Livewire;
 
 use App\Models\Article;
+use Livewire\Attributes\Computed;
 use Livewire\WithPagination;
 
 class ArticleList extends AdminComponent {
 	use WithPagination;
 
 	public $showOnlyPublished = false;
+
+	#[Computed]
+	public function articles() {
+		$query = Article::query();
+
+		if ($this->showOnlyPublished) {
+			$query->where('published', 1);
+		}
+
+		return $query->paginate(10, pageName: 'articles-page');
+	}
 
 	public function delete(Article $article) {
 		$article->delete();
