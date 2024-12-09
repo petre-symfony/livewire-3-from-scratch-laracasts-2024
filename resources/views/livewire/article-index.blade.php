4de4fbe13829621ca7c344eaa4b3dedc5347d624
petrero <petrero@gmail.com>
2024-12-09 12:47:27 +0200

    27.1 Offline States  It's inevitable, but at some point in time, we will be faced with an offline device. And as a user, that can be very frustrating to be offline and not really know it until you start clicking or tapping on something and then nothing works. So ideally, we would provide users some information when their device is offline, and we can do so with the wire offline attribute. The most basic usage is just applying that attribute to an element. By default, this element will be hidden and it will only appear when the device is offline.  So we are currently online. If we work offline, we can see our message, you are offline.

--- resources/views/livewire/article-index.blade.php
+++ resources/views/livewire/article-index.blade.php
@@ -1,4 +1,8 @@
 <div class="m-auto w-1/2">
+  <div wire:offline>
+    You are offline
+  </div>
+
   @foreach($articles as $article)
     <div class="mt-5 p-3" wire:key="{{ $article->id }}">
       <h3 class="text-2xl text-blue-500 mb-3 hover:text-blue-700">
