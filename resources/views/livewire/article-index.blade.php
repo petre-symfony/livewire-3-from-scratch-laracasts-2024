c2856e4607662cab4fdb11f366ba08a8e6e22f8b
petrero <petrero@gmail.com>
2024-12-09 13:00:52 +0200

    27.4 Now there are a couple of other modifiers, like for example, if we can add an attribute, it almost goes without saying that we can remove an attribute. So that if the browser detects that we are offline, then we can remove an attribute from an element. And likewise, if we can remove a CSS class, then it almost goes without saying that we can add a CSS class. All we have to do is use the class modifier, assign whatever classes that we want, and then when the browser detects it is offline, those classes are applied to the element. So the offline feature is very simple and straightforward.  The most basic usage is to just apply the wire offline attribute to an element, and when the browser detects that it is offline, that element will be displayed. But you can also use that attribute to add or remove classes and add or remove attributes. You'll just need to use the class and attribute modifiers to get your desired results.

--- resources/views/livewire/article-index.blade.php
+++ resources/views/livewire/article-index.blade.php
@@ -7,6 +7,7 @@
     <div class="mt-5 p-3" wire:key="{{ $article->id }}">
       <h3 class="text-2xl text-blue-500 mb-3 hover:text-blue-700"
           wire:offline.class.remove="text-blue-500 hover:text-blue-700"
+          wire:offline.class.remove="bg-red-500"
       >
         <a href="/articles/{{ $article->id }}">{{ $article->title }}</a>
       </h3>
