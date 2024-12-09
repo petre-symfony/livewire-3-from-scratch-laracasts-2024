5c428d374129d6b8235f9a8b38788d86e84cfa11
petrero <petrero@gmail.com>
2024-12-09 12:51:43 +0200

    27.2 But I want to provide a little more from a visual standpoint, like, for example, the links for the articles. We can provide a visual cue to change the color of those links, and we can do so by using the class modifier, which also has a remove modifier. The idea being that we can specify the classes that we want to remove from the element when we are offline. So in this case, I'm going to remove both the color and the hover effect. So that now when we go offline, not only do we get a message saying you are offline, but now we have this nice visual cue leading the user to think, huh, something is definitely not right.

--- resources/views/livewire/article-index.blade.php
+++ resources/views/livewire/article-index.blade.php
@@ -5,7 +5,9 @@
 
   @foreach($articles as $article)
     <div class="mt-5 p-3" wire:key="{{ $article->id }}">
-      <h3 class="text-2xl text-blue-500 mb-3 hover:text-blue-700">
+      <h3 class="text-2xl text-blue-500 mb-3 hover:text-blue-700"
+          wire:offline.class.remove="text-blue-500 hover:text-blue-700"
+      >
         <a href="/articles/{{ $article->id }}">{{ $article->title }}</a>
       </h3>
       <p>{{ str($article->content)->words(30) }}</p>
