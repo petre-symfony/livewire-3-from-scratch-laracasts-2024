3923e49c17360e0bc18ef94e356ada941be79135
petrero <petrero@gmail.com>
2024-12-09 10:00:30 +0200

    24.7 Now we just need to do something whenever we have that photo. So we'll have an if statement. If our photo is truthy, then we want to do something with that photo. But we do need to define that photo property, don't we? So we'll have public photo. And we can go ahead and add our validation rules. So really what we want is the image validator. And we want to set a max size of, let's say, one meg.  So now whenever we attempt to update this article, if we have a photo, then we will get the photo path by calling a store method on the photo object. So we can call store, and then we can specify the directory where we want to store it. So let's say article photos.  But we do want to be sure to store this in a place where we can publicly show the article. So I'm going to call this method called store publicly, and then we will specify the disk of where we want to store, which will be the public disk.

--- app/Livewire/Forms/ArticleForm.php
+++ app/Livewire/Forms/ArticleForm.php
@@ -16,6 +16,8 @@ class ArticleForm extends Form {
 	public $title;
 	#[Validate('required')]
 	public $content;
+	#[Validate('image|max:1024')]
+	public $photo;
 
 	public $published = false;
 	public $notifications = [];
@@ -42,6 +44,7 @@ class ArticleForm extends Form {
 			$this->notifications = [];
 		}
 
+		
 		Article::create($this->only(['title', 'content', 'published', 'notifications']));
 
 		cache()->forget('published-count');
@@ -52,6 +55,10 @@ class ArticleForm extends Form {
 
 		if (!$this->allowNotifications) {
 			$this->notifications = [];
+		}
+
+		if ($this->photo) {
+			$this->photo_path = $this->photo->storePublicly('article_photos', ['disk' => 'public']);
 		}
 
 		$this->article->update(
