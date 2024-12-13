8c1570d70d5530c5092c82982148119ca6ea31ad
petrero <petrero@gmail.com>
2024-12-13 09:47:07 +0200

    29.1 Handling Authentication  I feel like we need to talk about authentication, because we have talked about using components as our pages. We haven't used any controllers, and the typical Laravel applications use a controller to well do everything. So let's implement a login page for our application, but we're going to do so using Livewire components.  And so we're going to start inside of our routes, because I want to essentially take everything that is considered part of the dashboard, and I want to protect that. So we're going to finally uncomment our protected routes, but let's simplify this. Let's just say that this is going to be protected with the auth middleware. We will replace all of the routes inside of this closure for the group, but let's give this route a name of dashboard.  And while we're here, I also want to name our default route. That displays our article index, and I want to change this, but not necessarily change it. But let's just call it home, something that's nice and easy. Let's clean this up a little bit, and okay, I'm happy with that. We're going to leave this file open, because we will be coming back to define a route for signing out.

--- routes/web.php
+++ routes/web.php
@@ -14,21 +14,14 @@ Route::get('/', function () {
 });
 
 //Route::get('/search', Search::class);
-Route::get('/', ArticleIndex::class);
+Route::get('/', ArticleIndex::class)->name('home');
 Route::get('/articles/{article}', ShowArticle::class);
-Route::get('/dashboard', Dashboard::class);
-Route::get('/dashboard/articles', ArticleList::class)->name('dashboard.articles.index');
-Route::get('/dashboard/articles/create', CreateArticle::class);
-Route::get('/dashboard/articles/{article}/edit', EditArticle::class);
 
-/**
- Route::middleware([
-	'auth:sanctum',
-	config('jetstream.auth_session'),
-	'verified',
+Route::middleware([
+	'auth'
 ])->group(function () {
-	Route::get('/dashboard', function () {
-		return view('dashboard');
-	})->name('dashboard');
+	Route::get('/dashboard', Dashboard::class)->name('dashboard');
+	Route::get('/dashboard/articles', ArticleList::class)->name('dashboard.articles.index');
+	Route::get('/dashboard/articles/create', CreateArticle::class);
+	Route::get('/dashboard/articles/{article}/edit', EditArticle::class);
 });
- */
