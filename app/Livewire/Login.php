42a30210066169e6964f84075e96672f2729c450
petrero <petrero@gmail.com>
2024-12-13 09:59:59 +0200

    29.4 Of course, we don't see anything for our login, because we haven't implemented anything. So let's open up the view, let's also open up our class. And logging in is pretty simple.  We need an email, so we'll have our email property. Let's initialize that as an empty string, and then we also need the password. And we also want to validate these things as well. So for the email, we want to validate to ensure that it is required, but we also want to be sure that it is an actual email address. Now, of course, this isn't going to check if this is an actual valid email. I can type in something that will look like an email address. And really, that's all that we need in this particular case. We don't want anything more complex than that. And then for the password, we want to just ensure that we have a password to work with.

--- app/Livewire/Login.php
+++ app/Livewire/Login.php
@@ -2,9 +2,14 @@
 
 namespace App\Livewire;
 
+use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 class Login extends Component {
+	#[Validate('required|email')]
+	public $email = '';
+	public $password = '';
+
 	public function render() {
 		return view('livewire.login');
 	}
